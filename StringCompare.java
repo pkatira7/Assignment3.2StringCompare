27dd967eb44693707cf6dbbbd90cbc4ce13a791f
pkatira7 <pkatira@vmware.com>
2017-01-10 16:54:47 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,18 @@
+
+public class StringCompare {
+
+	public static void main(String[] args) {
+
+		
+	String	s1 = "xyz";
+    String  s2 = "xyz";
+    
+   if (s1==s2) 
+    {
+	 
+	   boolean same= true;
+	System.out.println(same);
+    }
+	}
+
+}
